18a8668980b24058b85675b40ab89b490f32605e
MPV <victor.sollerhed@gmail.com>
2010-02-07 12:02:40 +0100

    Readme fix.

---
+++ README.md
@@ -0,0 +1,5 @@
+Bignetwork Bong System
+======================
+
+Vårt webbsystem för att hantera försäljning i kiosken under våra evenemang. 
+
--- app/models/cart_row.rb
+++ app/models/cart_row.rb
@@ -1,4 +1,6 @@
 class CartRow < ActiveRecord::Base
   belongs_to :cart
   belongs_to :product_type
+  
+  validates_numericality_of :quantity
 end
