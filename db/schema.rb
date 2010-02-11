949c8b44943057de95c099d7905f4005b2a3e376
MPV <victor.sollerhed@gmail.com>
2010-02-11 21:35:36 +0100

    Minor CSS and menu fix.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100207144256) do
+ActiveRecord::Schema.define(:version => 20100211193038) do
 
   create_table "cart_rows", :force => true do |t|
     t.integer  "cart_id"
