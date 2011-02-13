ac49b44fb431824e01b698779f1c57ff0b7dd2be
MPV <victor.sollerhed@gmail.com>
2011-02-13 15:12:00 +0100

    Avoiding reset counters since they appear not to work.

--- db/migrate/20110213113533_add_products_count_to_deliveries.rb
+++ db/migrate/20110213113533_add_products_count_to_deliveries.rb
@@ -2,10 +2,11 @@ class AddProductsCountToDeliveries < ActiveRecord::Migration
   def self.up
     add_column :deliveries, :products_count, :integer, :default => 0
     
-    Delivery.reset_column_information
-    Delivery.find_each do |delivery|
-      Delivery.reset_counters delivery.id, :products
-    end
+    # Reset counters
+    #Delivery.reset_column_information
+    #Delivery.find_each do |delivery|
+    #  Delivery.reset_counters delivery.id, :products
+    #end
   end
 
   def self.down
--- db/migrate/20110213113857_add_products_count_to_purchases.rb
+++ db/migrate/20110213113857_add_products_count_to_purchases.rb
@@ -2,10 +2,11 @@ class AddProductsCountToPurchases < ActiveRecord::Migration
   def self.up
     add_column :purchases, :products_count, :integer, :default => 0
     
-    Purchase.reset_column_information
-    Purchase.find_each do |purchase|
-      Purchase.reset_counters purchase.id, :products
-    end
+    # Reset counters
+    #Purchase.reset_column_information
+    #Purchase.find_each do |purchase|
+    #  Purchase.reset_counters purchase.id, :products
+    #end
   end
 
   def self.down
